ba27d48946e058664922fecce39b1a42a5846514
Azur18 <azurcomeau@gmail.com>
2025-11-02 15:37:28 -0500

    Dictionnaire
    Interface dictionnaire regarde si mot est valide pendant qu'il est ecrit

--- dictionnairePage.py
+++ dictionnairePage.py
@@ -11,26 +11,15 @@ class DictionnairePage(ctk.CTkFrame):
 
         self.grid(row=0, column=0, padx=10, pady=10, sticky="nsew")
 
-        self.textbox = ctk.CTkTextbox(self, height=20)
+        self.textbox = ctk.CTkTextbox(self, height=10)
         self.textbox.grid(row=0, column=0, sticky="nsew")
-        self.textbox.insert("0.0", "Find A Word")
-
-        self.enterButton = ctk.CTkButton(self, text="Enter", command=self.readText)
-        self.enterButton.grid(row=0, column=1, pady=(20,10))
+        self.textbox.insert("0.0", "Entrez mot")
+        self.textbox.bind('<KeyRelease>', command = self.readText)
+        self.textbox.bind('<Key>', command = self.readText)
         
-        self.tk_textbox = ctk.CTkTextbox(self, activate_scrollbars=False)
-        self.tk_textbox.grid(row=0, column=2, sticky="nsew")
+        self.result_box = ctk.CTkLabel(self, text = 'mot invalide')
+        self.result_box.grid(row=1, column=0, sticky="nsew")
 
-
-        
-        
-        self.ctk_textbox_scrollbar = ctk.CTkScrollbar(self, command=self.tk_textbox.yview)
-        self.ctk_textbox_scrollbar.grid(row=0, column=3, sticky="ns")
-
-        self.tk_textbox.configure(yscrollcommand=self.ctk_textbox_scrollbar.set)
-
-    def readText(self):
-        #use find_valid_words
+    def readText(self, event):
         self.text = self.textbox.get("0.0", "end")
-        self.tk_textbox.delete("0.0", "end")
-        self.tk_textbox.insert("0.0", Dictionary.is_word_valid(Dictionary, tuple(self.text[:-1])))
+        self.result_box.configure(text = 'mot valide' if Dictionary.is_word_valid(Dictionary, tuple(self.text[:-1].upper())) else 'mot invalide')
